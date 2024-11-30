6a023c7a24c16fb0f6e70cccfa35e685654d6ff6
manishrohila <manishrohila0507@gmail.com>
2024-11-30 23:21:35 +0530

    Added: employee wage problem solution

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,36 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program ");
 
+        Scanner scanner = new Scanner(System.in);
+
+        // Taking input from the user
+        System.out.println("Enter the Wage per Hour: ");
+        int wagePerHour = scanner.nextInt();
+
+        System.out.println("Enter Full Day Working Hours: ");
+        int fullDayHour = scanner.nextInt();
+
+        System.out.println("Enter Part-Time Working Hours: ");
+        int partTimeHour = scanner.nextInt();
+
+        System.out.println("Enter Total Working Days in a Month: ");
+        int workingDaysPerMonth = scanner.nextInt();
+
+        System.out.println("Enter Maximum Working Hours in a Month: ");
+        int maxWorkingHours = scanner.nextInt();
+
+        // Passing inputs to the calculator
+        EmployeeWageCalculator calculator = new EmployeeWageCalculator(
+                wagePerHour, fullDayHour, partTimeHour, workingDaysPerMonth, maxWorkingHours
+        );
+
+        calculator.calculateTotalWages();
+
+        scanner.close();
     }
 }
